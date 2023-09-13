5fe4cd7756acb1d6cf5965c7c96b6efb29088629
David Simon Tetruashvili <dtetruash@proton.me>
2023-09-13 16:25:14 +0200

    Add final files before sweep

--- light_mlp/light_mlp_training.py
+++ light_mlp/light_mlp_training.py
@@ -35,13 +35,23 @@ device = (
     else "cpu"
 )
 
+def get_current_step(epoch, batch, batch_size, batches_per_epoch):
+    return batch_size * (batches_per_epoch * epoch + batch + 1)
 
 # Get the dataloader
-def get_dataloader(batch_size, split='train'):
-    is_train = split == 'train'
+def get_dataloader(batch_size, split='train', subset_fraction=1):
     "Get a dataloader for training or testing"
-    full_dataset = rd.RasterDataset(split)  # uses a config to decide what to load.
-    loader = torch.utils.data.DataLoader(dataset=full_dataset,
+
+    is_train = split == 'train'
+
+    full_dataset = rd.RasterDataset(split)
+
+    if subset_fraction > 1:
+        sub_dataset = torch.utils.data.Subset(full_dataset, indices=range(0, len(full_dataset), subset_fraction))
+    else:
+        sub_dataset = full_dataset
+
+    loader = torch.utils.data.DataLoader(dataset=sub_dataset,
                                          batch_size=batch_size,
                                          shuffle=is_train,
                                          pin_memory=True, num_workers=0)
@@ -62,7 +72,6 @@ def get_model(num_feats, num_layers, layer_size, dropout):
 
 def validate_model(model, valid_dl, loss_func, log_images=False, batch_idx=0):
     "Compute performance of the model on the validation dataset and log a wandb.Table"
-    model.eval()
     val_loss = 0.
     with torch.inference_mode():
         for step, (feats, target_vector) in tqdm(enumerate(valid_dl),
@@ -81,6 +90,7 @@ def validate_model(model, valid_dl, loss_func, log_images=False, batch_idx=0):
 def generate_validation_image(model, valid_dataset):
     """Generate an image comparing a ground truth image with one generated using the model.
     model: MLP which outputs light direction vectors"""
+    model.eval()
     with torch.inference_mode():
         # Randomly choose which image from the validation set to reconstruct
         image_number = np.random.randint(valid_dataset.num_frames)
@@ -145,12 +155,12 @@ def generate_validation_image(model, valid_dataset):
         return image_array, image_caption
 
 
-def train_epoch(epoch, train_dl, model, loss_func, optimizer, n_steps_per_epoch):
+def train_epoch(epoch, train_dl, model, loss_func, optimizer, n_batches_per_epoch):
     cumu_loss = 0.0
-    for step, (feats, target_vector) in tqdm(enumerate(train_dl),
-                                             total=len(train_dl),
-                                             desc="Step",
-                                             position=1, leave=False, colour='red'):
+    for batch, (feats, target_vector) in tqdm(enumerate(train_dl),
+                                              total=len(train_dl),
+                                              desc="Batch",
+                                              position=1, leave=False, colour='red'):
         # Move to device
         feats, target_vector = feats.to(device), target_vector.to(device)
 
@@ -167,10 +177,12 @@ def train_epoch(epoch, train_dl, model, loss_func, optimizer, n_steps_per_epoch)
 
         # Collect metrics
         metrics = {"train/train_loss": train_loss,
-                   "train/epoch": (step + 1 + (n_steps_per_epoch * epoch)) / n_steps_per_epoch,
+                   "train/epoch": (batch + 1 + (n_batches_per_epoch * epoch)) / n_batches_per_epoch,
                    }
 
-        if step + 1 < n_steps_per_epoch:
+        # step = get_current_step(epoch, batch, train_dl.batch_size, n_batches_per_epoch)
+
+        if batch + 1 < n_batches_per_epoch:
             # 🐝 Log train metrics to wandb
             wandb.log(metrics)
 
@@ -211,11 +223,11 @@ def carry_out_training():
 
         print(f"config object was: {list(config.items())}")
 
-        train_dl = get_dataloader(batch_size=config.batch_size)
-        print("Loaded train dataset.")
+        train_dl = get_dataloader(batch_size=config.batch_size, subset_fraction=config.data_subset_fraction)
+        print(f"Loaded train dataset with {len(train_dl)} batches and {len(train_dl.dataset)} samples.")
 
         valid_dl = get_dataloader(batch_size=2*config.batch_size, split='val')
-        print("Loaded validation dataset.")
+        print(f"Loaded valid dataset with {len(valid_dl)} batches and {len(valid_dl.dataset)} samples.")
 
         # make output dirs
         if not os.path.exists(config.model_checkpoint_path):
@@ -237,7 +249,7 @@ def carry_out_training():
 
         # +
         # Training
-        for epoch in tqdm(range(config.epochs) + 1,
+        for epoch in tqdm(range(config.epochs + 1),
                           desc="Epoch",
                           total=config.epochs,
                           position=0, leave=True, colour='green'):
@@ -249,6 +261,7 @@ def carry_out_training():
             wandb.log({"train/avg_loss": avg_train_loss})
 
             # Validation
+            model.eval()
             print(f"Validating model after epoch {epoch}")
             val_loss = validate_model(model, valid_dl, loss_func)
             print("Done validating.")
@@ -259,12 +272,13 @@ def carry_out_training():
             val_image = wandb.Image(val_image_array, caption=image_caption)
 
             # 🐝 Log train and validation metrics to wandb
-            val_metrics = {"val/val_loss": val_loss,
+            val_metrics = {"validation_loss": val_loss,
                            "val/images": val_image}
             wandb.log(val_metrics)
 
             print(f"Train Loss: {avg_train_loss:.3f} Valid Loss: {val_loss:3f}")
 
+            # Save model artifacts
             to_save = {
                 'model_state_dict': model.state_dict(),
                 'optimized_state_dict': optimizer.state_dict(),
@@ -334,7 +348,8 @@ if __name__ == "__main__":
 
     # non-sweep parameters
     sweep_configuration['parameters'].update({
-        'epochs': {'value': 20},
+        'epochs': {'value': 3},
+        'data_subset_fraction': {'value': 64},
         'model_checkpoint_path': {'value': 'model_checkpoints'},
         'model_trained_path': {'value': 'model_trained'},
         'scene': {'value': raster_config['paths']['scene']}
@@ -344,4 +359,4 @@ if __name__ == "__main__":
 
     sweep_id = wandb.sweep(sweep=sweep_configuration, project=project_name)
 
-    wandb.agent(sweep_id, function=carry_out_training, count=20)
+    wandb.agent(sweep_id, function=carry_out_training, count=15)
---
+++ light_mlp/model.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn.functional as F
+import torchvision
+
+
+class LightMLP(torchvision.ops.MLP):
+    def __init__(self, num_feats, hidden_channels, norm_layer=None, activation_layer=torch.nn.modules.activation.ReLU, inplace=None, bias=True, dropout=0.0):
+        super().__init__(num_feats * 3, hidden_channels + [3], norm_layer, activation_layer, inplace, bias, dropout)
+
+    def forward(self, x):
+        x = super().forward(x)
+        x = F.normalize(x)
+        assert x.shape[-1] == 3
+        return x
