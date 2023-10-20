f15655a08907d32938f7f82d9bee936b7d30758a
David Simon Tetruashvili <dtetruash@proton.me>
2023-10-20 19:19:05 +0200

    Add SH on sphere visualizer.

--- spherical_harmonics/visualize.py
+++ spherical_harmonics/visualize.py
@@ -3,6 +3,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 from icecream import ic
 from spherical_harmonics import render_second_order_SH, get_SH_alpha
+from matplotlib import cm, colors  # noqa: F401
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401
 
 
 def generate_harmonic_sphere_image(sh_coeffs, camera_transform):
@@ -29,14 +31,7 @@ def generate_harmonic_latlong_image(sh_coeffs, image_dim=100):
     Raises:
         NotImplementedError: [TODO:throw]
     """
-    phi = np.linspace(0, np.pi, image_dim)
-    theta = np.linspace(0, 2 * np.pi, image_dim)
-    phi, theta = np.meshgrid(phi, theta)
-    ic(theta.shape, phi.shape)
-
-    x = np.sin(phi) * np.cos(theta)
-    y = np.sin(phi) * np.sin(theta)
-    z = np.cos(phi)
+    x, y, z = get_sphere_surface_cartesian(image_dim)
     cart_normals = np.stack([x, y, z]).transpose((1, 2, 0))
     ic(cart_normals.shape)
     cart_normals = cart_normals.reshape(-1, 3)
@@ -63,15 +58,44 @@ def generate_harmonic_latlong_image_scipy(m, n, image_dim=100):
         return fcolors.imag
 
 
-if __name__ == "__main__":
+def get_sphere_surface_polar(resolution=100):
+    phi = np.linspace(0, np.pi, resolution)
+    theta = np.linspace(0, 2 * np.pi, resolution)
+    phi, theta = np.meshgrid(phi, theta)
+    return phi, theta
+
+
+def get_sphere_surface_cartesian(resolution=100):
+    phi, theta = get_sphere_surface_polar(resolution)
+    x = np.sin(phi) * np.cos(theta)
+    y = np.sin(phi) * np.sin(theta)
+    z = np.cos(phi)
+    return x, y, z
+
+
+def visualie_SH_on_3D_sphere(sh_coeffs):
+    x, y, z = get_sphere_surface_cartesian()
+    cart_normals = np.stack([x, y, z]).transpose((1, 2, 0))
+    # Calculate the spherical harmonic Y(l,m) and normalize to [0,1]
+    fcolors = render_second_order_SH(sh_coeffs, cart_normals, torch_mode=False)
+    fmax, fmin = fcolors.max(), fcolors.min()
+    fcolors = (fcolors - fmin) / (fmax - fmin)
+
+    # Set the aspect ratio to 1 so our sphere looks spherical
+    fig = plt.figure(figsize=plt.figaspect(1.0))
+    ax = fig.add_subplot(111, projection="3d")
+    ax.plot_surface(x, y, z, rstride=1, cstride=1, facecolors=cm.seismic(fcolors))
+    # Turn off the axis planes
+    ax.set_axis_off()
+    plt.show()
+
+
+def visualize_SH_validation_with_scipy():
     sh_coeffs = np.eye(9)
     fig, axes = plt.subplots(9, 3)
 
     ns = [0, 1, 1, 1, 2, 2, 2, 2, 2]
     ms = [0, -1, 0, 1, -2, -1, 0, 1, 2]
-
-    # axes.set_yticks(np.arange(len(ns)), labels=[f"n={n};m={m}" for n, m in zip(ns, ms)])
-
     for i in range(9):
         shading = generate_harmonic_latlong_image(sh_coeffs[i]) * (-1.0) ** ms[i]
         ax = axes[i, 0]
@@ -167,3 +191,8 @@ if __name__ == "__main__":
 
     # Show the plot
     plt.show()
+
+
+if __name__ == "__main__":
+    # axes.set_yticks(np.arange(len(ns)), labels=[f"n={n};m={m}" for n, m in zip(ns, ms)])
+    visualie_SH_on_3D_sphere(np.eye(9)[2])
