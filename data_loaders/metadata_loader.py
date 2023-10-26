544495bf0e42c1e5376f72cef519b4ac8ea17809
David Simon Tetruashvili <dtetruash@proton.me>
2023-10-26 21:11:55 +0200

    Add explicit metadata loaders

---
+++ data_loaders/metadata_loader.py
@@ -0,0 +1,19 @@
+import json
+
+import data_loaders.olat_render as ro
+from ile_utils.config import Config
+
+# TODO: move all metadata methods here
+
+
+def get_transform(frame_num, split="val"):
+    config = Config.get_config()
+    file_path = config.get("paths", "scene_path") + f"/transforms_{split}.json"
+    with open(file_path, "r") as tf:
+        frame_transforms = json.loads(tf.read())
+
+    return ro.get_c2w(frame_num, frame_transforms)
+
+
+def get_c2w(frame_num, split="val"):
+    return ro.to_rotation(get_transform(frame_num, split))
