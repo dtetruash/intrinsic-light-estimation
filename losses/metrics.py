7276d68118b866e87ee136ebb9b461ba5939617d
David Simon Tetruashvili <dtetruash@proton.me>
2023-11-17 16:33:19 +0100

    Fix MSE always zero, fix SIL2 always -ve

--- losses/metrics.py
+++ losses/metrics.py
@@ -3,12 +3,18 @@ for logging to a time-series but are not used for model training purposes.
 """
 
 import math
-from skimage.metrics import structural_similarity, mean_squared_error
+from skimage.metrics import (
+    structural_similarity,
+    mean_squared_error,
+    peak_signal_noise_ratio,
+)
 import numpy as np
 import torch
 
 
+
 def mse(img_pred, img_target):
+    """Uses the SK-image MSE function: MSE per-element, and not per-pixel"""
     return mean_squared_error(img_pred, img_target)
 
 
@@ -24,14 +30,21 @@ def psnr(mse):
     return -10.0 * math.log10(mse)
 
 
-def dssim(img_pred, img_target):
+def psnr_sk(pred, target):
+    # Checked!
+    return peak_signal_noise_ratio(target, pred, data_range=1.0)
+
+
+def dssim(img_pred, img_target, channel_axis=2):
+    # Believable, checked.
     """Compute the structural dissimilarity between two image arrays.
     Only supports single image pairs.
     """
-    return (1.0 - ssim(img_pred, img_target)) * 0.5
+    return (1.0 - ssim(img_pred, img_target, channel_axis=channel_axis)) * 0.5
 
 
 def ssim(img_pred, img_target, **kwargs):
+    # Checked
     """Compute the structural similarity between two image arrays.
     Only supports single image pairs.
     """
@@ -40,27 +53,27 @@ def ssim(img_pred, img_target, **kwargs):
 
 
 def scale_inv_L2(pred, target, lamb=0.5, torch_mode=True):
-    """Scale-invariant L2 loss (MSE)
-    Is only defined on values above zero. Will clip element-wise between 1e-3 and 1.0
+    # FIXME: ALWAYS -Ve
+    """Scale-invariant L2 loss (MSE). Assumes 2D input (W,H, ...)
+    Is only defined on values above zero. Will clip element-wise between 1e-5 and 1.0
     """
     assert pred.shape == target.shape, (
         f"Given {'tensors' if torch_mode else 'arrays'} "
         "were of differing shapes: {pred.shape, target.shape}."
     )
 
+    assert pred.dtype == np.float32, f"Was {pred.dtype}"
+    assert pred.min() >= 0.0, f"Was {pred.min()}"
+    assert pred.max() <= 1.0, f"Was {pred.max()}"
+
+    assert target.dtype == np.float32, f"Was {target.dtype}"
+    assert target.min() >= 0.0, f"Was {target.min()}"
+    assert target.max() <= 1.0, f"Was {target.max()}"
+
+    # ic(pred.shape, pred.dtype, pred.min(), pred.max())
+    # ic(target.shape, target.dtype, target.min(), target.max())
+
     lib = torch if torch_mode else np
-
-    dim = target.dim() if torch_mode else target.ndim
-
-    if dim == 2:  # (B, 3)
-        num_pixles = target.shape[0]
-    elif dim == 3:  # (W,H,3)
-        num_pixles = target.shape[0] * target.shape[1]
-    else:
-        raise ValueError(
-            f"Unsupported number of dimsnions in array/tensor {dim}. "
-            "Supported 2 (B, 3) or 3 (W,H,3)."
-        )
 
     # Clip and add eps to avoid -ve infs and nans after log
     eps = float(1e-5)
@@ -70,19 +83,22 @@ def scale_inv_L2(pred, target, lamb=0.5, torch_mode=True):
     # Get element-wize log difference
     diff = lib.log(clipped_pred) - lib.log(clipped_target)
 
+    num_pixles = target.shape[0] * target.shape[1]  # always assuming 2D input
     if torch_mode:
         assert isinstance(diff, torch.Tensor)
-        mse = torch.square(diff).mean()
+        mse = torch.sum(torch.square(diff)) / float(num_pixles)
     else:
-        mse = np.square(diff).mean()
+        mse = np.sum(np.square(diff)) / float(num_pixles)
 
     num_pixles_sqr = num_pixles * num_pixles
     scale_inv_mse = lib.square(diff.sum()) / float(num_pixles_sqr)
 
+    # ic("sil2:", mse, scale_inv_mse)
+
     return mse - lamb * scale_inv_mse
 
 
-def local_mse(image1, image2, window_size_factor=0.1):
+def local_mse(image1, image2, window_size_factor=0.1, window_step=None, use_sil2=False):
     """
     Compute local mean square error between two images with a specified window size.
     Note that consecutive windows do not overlap
@@ -98,25 +114,39 @@ def local_mse(image1, image2, window_size_factor=0.1):
 
     # Ensure the images have the same shape
     assert image1.shape == image2.shape, "Images must have the same shape"
+    # ic(image1.shape, image2.shape)
 
     # Compute window size based on the specified factor
     window_size = int(image1.shape[1] * window_size_factor)
+    if window_step is None:
+        window_step = window_size // 2
+
+    if window_step > window_size:
+        raise ValueError("The chosen window step is larger than the windiw size.")
 
     # Compute the local mean square error using a rolling window
-    loc_mse_accu = 0.0
-    window_ctn = 0
-
-    for i in range(0, image1.shape[0] - window_size + 1, window_size):
-        for j in range(0, image1.shape[1] - window_size + 1, window_size):
+    lmse_accu = 0.0
+    window_ctn = 0.0
+    all_lmse = []
+    for i in range(0, image1.shape[0] - window_size + 1, window_step):
+        for j in range(0, image1.shape[1] - window_size + 1, window_step):
             window1 = image1[i : i + window_size, j : j + window_size, :]
             window2 = image2[i : i + window_size, j : j + window_size, :]
 
-            mse = mean_squared_error(window1, window2)
+            # Checked to be 40, 40, 3 at float 32
+            # ic(window1.shape, window2.shape, window1.dtype, window2.dtype)
 
-            loc_mse_accu += mse
+            if use_sil2:
+                mse = scale_inv_L2(window1, window2, torch_mode=False)
+            else:
+                mse = mean_squared_error(window1, window2)
+                # ic(mse, np.all(window1 == window2))
+
+            lmse_accu += mse
             window_ctn += 1
 
-    return loc_mse_accu / window_ctn
+    lmse = lmse_accu / window_ctn
+    return lmse
 
 
 def lpips():
