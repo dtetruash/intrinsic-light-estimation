805f6b6557ba6e794e365545e4cffbde07ab778d
David Simon Tetruashvili <dtetruash@proton.me>
2023-11-17 23:49:39 +0100

    Add extended interpolation to configs

--- ile_utils/config.py
+++ ile_utils/config.py
@@ -29,7 +29,9 @@ class Config:
     @staticmethod
     def get_config(config_path=None):
         if Config.__conf is None:
-            Config.__conf = configparser.ConfigParser()
+            Config.__conf = configparser.ConfigParser(
+                interpolation=configparser.ExtendedInterpolation()
+            )
 
             if config_path is None:
                 config_path = Config.__config_path
@@ -38,22 +40,22 @@ class Config:
             Config.__loaded_path = config_path
 
             # Write in interpolated values.
-            Config.__conf.set("dataset", "scene_path", "%(dataset_path)s/%(scene)s")
-            Config.__conf.set("dataset", "data_path", "%(scene_path)s/%(split)s")
+            Config.__conf.set("dataset", "scene_path", "${dataset_path}/${scene}")
+            Config.__conf.set("dataset", "data_path", "${scene_path}/${split}")
 
             if Config.__conf.has_option("dataset", "split"):
                 Config.__conf.set(
                     "dataset",
                     "transforms_file",
-                    "%(scene_path)s/transforms_%(split)s.json",
+                    "${scene_path}/transforms_${split}.json",
                 )
             else:
                 Config.__conf.set(
-                    "dataset", "transforms_file", "%(scene_path)s/transforms.json"
+                    "dataset", "transforms_file", "${scene_path}/transforms.json"
                 )
 
             Config.__conf.set(
-                "dataset", "lights_file", "%(scene_path)s/lights_%(scene)s.json"
+                "dataset", "lights_file", "${scene_path}/lights_${scene}.json"
             )
 
         return Config.__conf
